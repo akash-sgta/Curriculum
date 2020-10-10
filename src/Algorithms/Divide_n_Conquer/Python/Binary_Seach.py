ca10e5ffdd9434730ee48fa925066c828bbf652b
akash_sgta <alphatrionakash@gmail.com>
2020-10-10 19:00:25 +0530

    pc commit intermediate

---
+++ src/Algorithms/Divide_n_Conquer/Python/Binary_Seach.py
@@ -0,0 +1,20 @@
+def binary_search(key, array, l, h):
+
+    if l <= h:
+        mid = l + (h-l)//2
+        if key == array[mid]:
+            return mid+1
+        elif key < array[mid]:
+            return binary_search(key, array, l, mid-1)
+        else:
+            return binary_search(key, array, mid+1, h)
+    return None
+
+def main():
+
+    array = list([9,1,8,2,7,3,4,6,5])
+    array.sort()
+    print(binary_search(2, array, 0, len(array)-1))
+    print(binary_search(21, array, 0, len(array)-1))
+
+main()
---
+++ src/Algorithms/Divide_n_Conquer/Python/Multiplication_of_two_large_numbers.py
@@ -0,0 +1,8 @@
+def multiply(a, b):
+    
+    pass
+
+def main():
+    print(multiply(487512468,154887954))
+
+main()
